0f5762aa4e120ba9827a6368599ea2aec378f675
Maarten <maartenhandels@gmail.com>
2022-10-11 20:00:27 +0100

    Remove customer class

--- src/main/java/com/pensionmaite/pensionmaitebackend/entity/Customer.java
+++ src/main/java/com/pensionmaite/pensionmaitebackend/entity/Customer.java
@@ -1,31 +1,31 @@
-package com.pensionmaite.pensionmaitebackend.entity;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.util.Set;
-
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-public class Customer {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    private String name;
-
-    private String lastName;
-
-    private String email;
-
-    private String phone;
-
-    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private Set<Reservation> reservations;
-}
+//package com.pensionmaite.pensionmaitebackend.entity;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//import java.util.Set;
+//
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Entity
+//public class Customer {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//
+//    private String name;
+//
+//    private String lastName;
+//
+//    private String email;
+//
+//    private String phone;
+//
+//    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//    private Set<Reservation> reservations;
+//}
