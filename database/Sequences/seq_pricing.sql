e50fd0782714b62ecbfc2153c444bd49441440db
Maarten <maartenhandels@gmail.com>
2023-04-11 22:48:12 +0100

    Add sql for sequences

---
+++ database/Sequences/seq_pricing.sql
@@ -0,0 +1,3 @@
+DROP SEQUENCE IF EXISTS pensionmaite.seq_pricing cascade;
+
+CREATE SEQUENCE pensionmaite.seq_pricing START WITH 1;
---
+++ database/Sequences/seq_room_type.sql
@@ -0,0 +1,3 @@
+DROP SEQUENCE IF EXISTS pensionmaite.seq_room_type cascade;
+
+CREATE SEQUENCE pensionmaite.seq_room_type START WITH 1;
