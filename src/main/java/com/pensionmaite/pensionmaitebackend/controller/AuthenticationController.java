983faae0f2f74368bc69b533e4328b24da3e5dcf
Maarten <maartenhandels@gmail.com>
2023-05-23 19:45:41 +0100

    Create AuthenticationController.java

---
+++ src/main/java/com/pensionmaite/pensionmaitebackend/controller/AuthenticationController.java
@@ -0,0 +1,39 @@
+package com.pensionmaite.pensionmaitebackend.controller;
+
+import com.pensionmaite.pensionmaitebackend.events.request.AuthenticationRequest;
+import com.pensionmaite.pensionmaitebackend.events.request.RegisterRequest;
+import com.pensionmaite.pensionmaitebackend.events.response.AuthenticationResponse;
+import com.pensionmaite.pensionmaitebackend.service.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/admin/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authenticationService.register(registerRequest));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest authenticationRequest) {
+        return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        authenticationService.refreshToken(request, response);
+    }
+}
